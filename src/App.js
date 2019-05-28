310fde6e651634cd69d3432f37073be2132b4679
Chris Bistline <chris.bistline@gmail.com>
2019-05-28 16:37:15 -0500

    header and carrousel built

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './resources/styles.css';
+
+import Header from './components/header_footer/Header'
+import Featured from './components/featured';
+
 
 function App() {
   return (
-    <div className="App">
-      Hello there
+    <div className="App" style={{ height:"1500px", background:"cornflowerblue"}}>
+      <Header />
+      <Featured />
     </div>
   );
 }
